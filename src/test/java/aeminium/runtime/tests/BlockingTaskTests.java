f44df11d9f9f261bb31ead852ddd01b855ae4bd7
mcarolinasaf <carolinasaf23@gmail.com>
2025-10-28 00:24:23 +0000

    Add new test

--- src/test/java/aeminium/runtime/tests/BlockingTaskTests.java
+++ src/test/java/aeminium/runtime/tests/BlockingTaskTests.java
@@ -96,4 +96,10 @@ public class BlockingTaskTests extends BaseTest {
 		rt.shutdown();
 		assertTrue( counter.get() == 2*TASK_COUNT );
 	}
+
+	@Test
+	public void testThatAlwaysFails() {
+		assertTrue(false);
+	}
+
 }
